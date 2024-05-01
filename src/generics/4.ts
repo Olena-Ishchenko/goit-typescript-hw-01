d6079ecab9484ef8005b9ce0147db8659ac9edef
Olena-Ishchenko <elena.dubovalova@gmail.com>
2024-05-01 23:14:12 +0300

    generics

---
+++ src/generics/4.ts
@@ -0,0 +1,22 @@
+type User1 = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+};
+
+function createOrUpdateUser(initialValues: Partial<User1>): User1 {
+  // Оновлення користувача
+  const defaultsUser: User1 = {
+    name: "",
+    surname: "",
+    email: "",
+    password: "",
+  };
+  return { ...defaultsUser, ...initialValues };
+}
+
+createOrUpdateUser({
+  email: "user@mail.com",
+  password: "password123",
+});
